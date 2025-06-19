b30955fc9b2701169bd18ad5a9d5e89f10186243
TLeslie06 <2511011@bdeb.qc.ca>
2025-06-19 12:46:42 -0400

    Update Exo_1.py
    update

--- Exo_1.py
+++ Exo_1.py
@@ -1,1 +1,30 @@
 import tkinter as tk
+
+def afficher_selection():
+    indices = liste.curselection()
+
+    print("Concepts sélectionnés:")
+    for i in indices:
+        element = liste.get(i)
+        print("-", element)
+
+window = tk.Tk()
+window.title("Concept de cybersecurité")
+window.geometry("300x400")
+window.configure(bg="yellow")
+
+liste = tk.Listbox(window, selectmode="multiple")
+liste.pack(pady=20)
+
+concepts= ["Phishing", "Malware", "Firewall", "Chiffrement", "VPN"]
+
+
+for concept in concepts:
+    liste.insert(tk.END, concept)
+
+bouton = tk.Button(window, text="Afficher", command=afficher_selection)
+bouton.pack()
+
+
+
+window.mainloop()
